5a1b40c68de8d4e873bc2450b07b1f094c5e630f
Veselin Tairski <tairskiv@vmware.com>
2019-12-23 22:04:16 +0200

    add town and days support

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const fetch = require('node-fetch');
 
-app.get("/url", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+const TOKEN = '?key=2dadaadd732f41a68ac91755192311';
+const BASE_API = 'http://api.worldweatheronline.com/premium/v1/weather.ashx';
+const JSON_FORMAT = '&format=json';
+const NUM_OF_DAYS = '&num_of_days=';
+
+app.get("/forecast", (req, res) => {
+  const town = req.query.town;
+  const daysPayload = req.query.days;
+
+  let days = 1;
+
+  if (daysPayload && daysPayload > 0 && daysPayload <= 15) {
+    days = daysPayload
+  }
+
+  const url = `${BASE_API}${TOKEN}&q=${town}${JSON_FORMAT}${NUM_OF_DAYS}${days}`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => res.send({ data }))
+  .catch(err => {
+    res.send('An error has occured', err);
+  });
 });
 
 
